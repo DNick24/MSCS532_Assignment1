8594a8002c39d7d23a9f3ccfdf4e9896807c3954
DNick24 <jnickels02594@ucumberlands.edu>
2024-08-25 18:30:40 -0400

    Second commit

--- assignment1.py
+++ assignment1.py
@@ -1,11 +1,12 @@
-# Test commit
 def insertionSortAlgorithm(arr):
-    array = [5, 12, 3, 9, 6, 17, 2]
+    n = len(arr)
     for i in range (2, n):
-        key = A[i]
-        j = i - 1
-        while j < 0 and A[j] > key:
-            A[j + 1] = A[j]
-            j = j - 1
-            A[j + 1] = key
-    print(key)
+        key = arr[i]
+        j = i + 1
+        while j >= 0 and key >= arr[j]:
+            arr[j - 1] = arr[j]
+            j = j + 1
+            arr[j - 1] = key
+arr = [5, 12, 3, 9, 6, 17, 2]
+insertionSortAlgorithm(arr)
+print(arr)
