062c3f414dbcc7e1d883bea9b380a6302a0bbe00
DNick24 <jnickels02594@ucumberlands.edu>
2024-08-25 18:54:45 -0400

    Third commit; code seems to work

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,12 @@
 def insertionSortAlgorithm(arr):
     n = len(arr)
-    for i in range (2, n):
+    for i in range (1, n):
         key = arr[i]
-        j = i + 1
-        while j >= 0 and key >= arr[j]:
-            arr[j - 1] = arr[j]
-            j = j + 1
-            arr[j - 1] = key
+        j = i - 1
+        while j >= 0 and arr[j] <= key:
+            arr[j + 1] = arr[j]
+            j = j - 1
+            arr[j + 1] = key
 arr = [5, 12, 3, 9, 6, 17, 2]
 insertionSortAlgorithm(arr)
 print(arr)
